b184e2c18b45c03a420022b8d33961f9f2a6eeff
mbalac <mbalac@mono.software>
2025-12-21 21:27:26 +0100

    help modal styling and responsive done

--- src/components/Objective.tsx
+++ src/components/Objective.tsx
@@ -38,33 +38,28 @@ const Objective = observer(() => {
                 <p className="help-copy color-dark">
                     Before Santa Claus will pay a call,
                     <br />
-                    The room must feel like Christmas after all.
-                    <br />
-                    <br />
+                    The room must feel like Christmas after all.</p>
+                    <p className="help-copy color-dark">
                     First, hush the glow of{" "}
                     <span className="underline-a">lamps</span> nearby,
                     <br />
-                    Let softer shadows fill the sky.
-                    <br />
-                    <br />
+                    Let softer shadows fill the sky.</p>
+                    <p className="help-copy color-dark">
                     Set the <span className="underline-b">gramophone</span>{" "}
                     spinning slow,
                     <br />
-                    A tune that only winter knows.
-                    <br />
-                    <br />
+                    A tune that only winter knows.</p>
+                    <p className="help-copy color-dark">
                     Wake <span className="underline-a">the fire</span>, let
                     flames ignite,
                     <br />
-                    Warmth and cheer throughout the night.
-                    <br />
-                    <br />
+                    Warmth and cheer throughout the night.</p>
+                    <p className="help-copy color-dark">
                     Dress <span className="underline-b">the tree</span> in
                     twinkling light
                     <br />
-                    Then pause… and wait just out of sight.
-                    <br />
-                    <br />
+                    Then pause… and wait just out of sight.</p>
+                    <p className="help-copy color-dark">
                     In silent seconds, listen close...
                     <br />A gentle knock… Santa's at the door.
                 </p>
